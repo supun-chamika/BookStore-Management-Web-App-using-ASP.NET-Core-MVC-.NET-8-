cc8790306a66678a674b1d2c2196f2b34a587171
mypermanenthelloworld1@gmail.com <mypermanenthelloworld1@gmail.com>
2024-02-07 10:20:44 +0530

    home and details pages

--- BookStore.WEB/Areas/Customer/Controllers/HomeController.cs
+++ BookStore.WEB/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,18 +9,27 @@ namespace BookStore.WEB.Areas.Customer.Controllers
 	public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
         }
 
-        public IActionResult Privacy()
+		public IActionResult Details(int productId)
+		{
+			Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+			return View(product);
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
